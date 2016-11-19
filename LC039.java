6b22d29b5b0bffc612fb2e89331e664456aced95
Wenting Shi <alexshi1991@gmail.com>
2016-11-19 10:46:53 -0800

    update leetcode#039

--- LC039.java
+++ LC039.java
@@ -3,7 +3,8 @@
  *
  * Idea:   1. backtracking
  *         2. sort the candidates array first
- *         3. each recursive call only considers from candidates[level...]
+ *         3. each recursive call only considers items from candidates[begin...]
+ *         
  */
 
 public class Solution {
@@ -14,16 +15,16 @@ public class Solution {
         }
         List<Integer> currList = new ArrayList<Integer>();
         Arrays.sort(candidates);
-        int level = 0;
-        combinationSumRecur(level, candidates, result, currList, target);
+        int begin = 0;
+        combinationSumRecur(begin, candidates, result, currList, target);
         return result;
     }
     
-    private void combinationSumRecur(int level, int[] candidates, List<List<Integer>> result, List<Integer> currList, int target) {
+    private void combinationSumRecur(int begin, int[] candidates, List<List<Integer>> result, List<Integer> currList, int target) {
         if (target == 0) {
             result.add(new ArrayList<Integer>(currList));
         }
-        for (int i = level; i < candidates.length; i++) {
+        for (int i = begin; i < candidates.length; i++) {
             int num = candidates[i];
             if (num <= target) {
                 currList.add(num);
