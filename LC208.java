9a4c819d3525344d5b38b7a99dc43640f2195eda
Wenting Shi <alexshi1991@gmail.com>
2017-01-03 15:44:16 -0800

    leetcode#208

---
+++ LC208.java
@@ -0,0 +1,93 @@
+/*
+ * Problem:   https://leetcode.com/problems/implement-trie-prefix-tree/
+ *
+ * Idea:      familiar with the definition of Trie
+ *
+ */
+
+class TrieNode {
+    
+    private TrieNode[] links;
+
+    private final int R = 26;
+
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch -'a'] != null;
+    }
+    public TrieNode get(char ch) {
+        return links[ch -'a'];
+    }
+    public void put(char ch, TrieNode node) {
+        links[ch -'a'] = node;
+    }
+    public void setEnd() {
+        isEnd = true;
+    }
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                return false;
+            } else {
+                if (i != (word.length() - 1)) {
+                    node = node.get(currentChar);
+                } else {
+                    return node.get(currentChar).isEnd();
+                }
+            }
+        }
+        return false;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char currentChar = prefix.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                return false;
+            }
+            node = node.get(currentChar);
+        }
+        return true;
+    }
+}
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie = new Trie();
+// trie.insert("somestring");
+// trie.search("key");
